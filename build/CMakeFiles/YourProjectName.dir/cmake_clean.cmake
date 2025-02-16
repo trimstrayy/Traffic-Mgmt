2c0942fcdb4ce0f222051d4d48fec1dbeafa03bf
trimstrayy <ayush11dahal@gmail.com>
2025-02-16 21:30:54 +0545

    setup completed, and simutlator running

---
+++ build/CMakeFiles/YourProjectName.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/YourProjectName.dir/src/main.cpp.obj"
+  "CMakeFiles/YourProjectName.dir/src/main.cpp.obj.d"
+  "YourProjectName.exe"
+  "YourProjectName.exe.manifest"
+  "YourProjectName.pdb"
+  "libYourProjectName.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/YourProjectName.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/YourProjectName.dir/src/main.cpp.obj.d
@@ -0,0 +1,84 @@
+CMakeFiles/YourProjectName.dir/src/main.cpp.obj: \
+ D:\KU\3rd\ SEMESTER\DSA\ COMP\ 202\Traffic-Mgmt\src\main.cpp \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_stdinc.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_platform_defines.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.1.0/include/stdarg.h \
+ C:/msys64/mingw64/include/stdarg.h \
+ C:/msys64/mingw64/include/_mingw_stdarg.h \
+ C:/msys64/mingw64/include/vadefs.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.1.0/include/stdint.h \
+ C:/msys64/mingw64/include/stdint.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/corecrt.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.1.0/include/stddef.h \
+ C:/msys64/mingw64/include/stddef.h C:/msys64/mingw64/include/string.h \
+ C:/msys64/mingw64/include/sec_api/string_s.h \
+ C:/msys64/mingw64/include/wchar.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/corecrt_wstdlib.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/_mingw_stat64.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/wchar_s.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_begin_code.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_close_code.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_assert.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_asyncio.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_atomic.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_audio.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_endian.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_error.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_mutex.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_thread.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_properties.h \
+ C:/msys64/mingw64/include/process.h \
+ C:/msys64/mingw64/include/corecrt_startup.h \
+ C:/msys64/mingw64/include/sys/types.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_iostream.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_bits.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_blendmode.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_camera.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_pixels.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_surface.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_rect.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_clipboard.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_cpuinfo.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_dialog.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_video.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_events.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_gamepad.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_guid.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_joystick.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_power.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_sensor.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_keyboard.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_keycode.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_scancode.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_mouse.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_pen.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_touch.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_filesystem.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_gpu.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_haptic.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_hidapi.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_hints.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_init.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_loadso.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_locale.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_log.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_messagebox.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_metal.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_misc.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_platform.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_process.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_render.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_storage.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_system.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_time.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_timer.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_tray.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_version.h \
+ D:/KU/3rd\ SEMESTER/DSA\ COMP\ 202/Traffic-Mgmt/libs/SDL3_install/include/SDL3/SDL_oldnames.h
